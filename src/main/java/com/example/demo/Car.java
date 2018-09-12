947d2be8e9775fdcb9b8d94446a5b4cee29814a5
matveenko73 <andreymatveenko3@gmail.com>
2018-09-12 21:38:17 +0300

    Отправка данных в базу данных

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String model;
+    private String productionYer;
+    private String brand;
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getProductionYer() {
+        return productionYer;
+    }
+
+    public void setProductionYer(String productionYer) {
+        this.productionYer = productionYer;
+    }
+
+}
---
+++ src/main/java/com/example/demo/CarPage.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+public class CarPage {
+    @Inject
+    private CarRepository carRepository;
+
+    private String model;
+    private String productionYer;
+    private String brand;
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getProductionYer() {
+        return productionYer;
+    }
+
+    public void setProductionYer(String productionYer) {
+        this.productionYer = productionYer;
+    }
+
+
+    public void save() {
+        Car car = new Car();
+        car.setBrand(brand);
+        car.setModel(model);
+        car.setProductionYer(productionYer);
+        car.setColor(color);
+        carRepository.save(car);
+    }
+
+    public List<Car> findAll() {
+        return carRepository.findAll();
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+}
