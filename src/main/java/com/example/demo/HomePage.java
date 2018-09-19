bbb592cd7354ec22e0fa311d09078c25be969ee7
matveenko73 <andreymatveenko3@gmail.com>
2018-09-19 21:36:25 +0300

    Начало проекта положено!

---
+++ src/main/java/com/example/demo/HomePage.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import javax.inject.Named;
+
+@Named
+public class HomePage {
+
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
