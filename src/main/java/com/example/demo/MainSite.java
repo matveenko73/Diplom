ba89e6527d1e635f443d607922d445bd90f5c0ee
matveenko73 <andreymatveenko3@gmail.com>
2018-10-04 21:48:06 +0300

    Вёрстка перовй пробной секции

--- src/main/java/com/example/demo/MainSite.java
+++ src/main/java/com/example/demo/MainSite.java
@@ -11,12 +11,27 @@ public class MainSite {
                 "C:\\Users\\Андрей\\Downloads\\geckodriver.exe");
 
         FirefoxDriver driver = new FirefoxDriver();
-        driver.get("http://rst.ua/oldcars/maserati/");
+        //driver.get("http://rst.ua/oldcars/maserati/");
+//        driver.get("http://rst.ua");
+        driver.get("http://rst.ua/oldcars/audi/");
 //        List<WebElement> prices = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-s-p");
-        List<WebElement> images = driver.findElementsByCssSelector("img.rst-ocb-i-i");
-        for (WebElement image : images) {
-            System.out.println((image.getAttribute("src")));
+//        List<WebElement> images = driver.findElementsByCssSelector("img.rst-ocb-i-i");
+//        for (WebElement image : images) {
+//            System.out.println((image.getAttribute("src")));
+//        }
+
+//        WebElement elementByCssSelector = driver.findElementByName("make[]");
+//        elementByCssSelector.click();
+
+//        List<WebElement> images = driver.findElementsByCssSelector(".rst-ocb-i-a");
+//        for (WebElement image : images) {
+//            System.out.println((image.getAttribute("src")));
+//        } ссылки
+
+        List<WebElement> pictures = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-b");
+        for (WebElement picture : pictures) {
+            System.out.println((picture.getAttribute("src")));
         }
-        driver.close();
+        //driver.close();
     }
 }
