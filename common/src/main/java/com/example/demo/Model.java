ec31e71aeceee3095780d048bfdf33a31075678b
matveenko73 <andreymatveenko3@gmail.com>
2018-10-08 18:56:13 +0300

    выделили модуль common

---
+++ common/src/main/java/com/example/demo/Model.java
@@ -0,0 +1,321 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String M1;
+    private String M2;
+    private String M3;
+    private String M4;
+    private String M5;
+    private String M6;
+    private String M7;
+    private String M8;
+    private String M9;
+    private String M10;
+    private String M11;
+    private String M12;
+    private String M13;
+    private String M14;
+    private String M15;
+    private String M16;
+    private String M17;
+    private String M18;
+    private String M19;
+    private String M20;
+    private String M21;
+    private String M22;
+    private String M23;
+    private String M24;
+    private String M25;
+    private String M26;
+    private String M27;
+    private String M28;
+    private String M29;
+    private String M30;
+    private String M31;
+    private String M32;
+    private String M33;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getM1() {
+        return M1;
+    }
+
+    public void setM1(String m1) {
+        M1 = m1;
+    }
+
+    public String getM2() {
+        return M2;
+    }
+
+    public void setM2(String m2) {
+        M2 = m2;
+    }
+
+    public String getM3() {
+        return M3;
+    }
+
+    public void setM3(String m3) {
+        M3 = m3;
+    }
+
+    public String getM4() {
+        return M4;
+    }
+
+    public void setM4(String m4) {
+        M4 = m4;
+    }
+
+    public String getM5() {
+        return M5;
+    }
+
+    public void setM5(String m5) {
+        M5 = m5;
+    }
+
+    public String getM6() {
+        return M6;
+    }
+
+    public void setM6(String m6) {
+        M6 = m6;
+    }
+
+    public String getM7() {
+        return M7;
+    }
+
+    public void setM7(String m7) {
+        M7 = m7;
+    }
+
+    public String getM8() {
+        return M8;
+    }
+
+    public void setM8(String m8) {
+        M8 = m8;
+    }
+
+    public String getM9() {
+        return M9;
+    }
+
+    public void setM9(String m9) {
+        M9 = m9;
+    }
+
+    public String getM10() {
+        return M10;
+    }
+
+    public void setM10(String m10) {
+        M10 = m10;
+    }
+
+    public String getM11() {
+        return M11;
+    }
+
+    public void setM11(String m11) {
+        M11 = m11;
+    }
+
+    public String getM12() {
+        return M12;
+    }
+
+    public void setM12(String m12) {
+        M12 = m12;
+    }
+
+    public String getM13() {
+        return M13;
+    }
+
+    public void setM13(String m13) {
+        M13 = m13;
+    }
+
+    public String getM14() {
+        return M14;
+    }
+
+    public void setM14(String m14) {
+        M14 = m14;
+    }
+
+    public String getM15() {
+        return M15;
+    }
+
+    public void setM15(String m15) {
+        M15 = m15;
+    }
+
+    public String getM16() {
+        return M16;
+    }
+
+    public void setM16(String m16) {
+        M16 = m16;
+    }
+
+    public String getM17() {
+        return M17;
+    }
+
+    public void setM17(String m17) {
+        M17 = m17;
+    }
+
+    public String getM18() {
+        return M18;
+    }
+
+    public void setM18(String m18) {
+        M18 = m18;
+    }
+
+    public String getM19() {
+        return M19;
+    }
+
+    public void setM19(String m19) {
+        M19 = m19;
+    }
+
+    public String getM20() {
+        return M20;
+    }
+
+    public void setM20(String m20) {
+        M20 = m20;
+    }
+
+    public String getM21() {
+        return M21;
+    }
+
+    public void setM21(String m21) {
+        M21 = m21;
+    }
+
+    public String getM22() {
+        return M22;
+    }
+
+    public void setM22(String m22) {
+        M22 = m22;
+    }
+
+    public String getM23() {
+        return M23;
+    }
+
+    public void setM23(String m23) {
+        M23 = m23;
+    }
+
+    public String getM24() {
+        return M24;
+    }
+
+    public void setM24(String m24) {
+        M24 = m24;
+    }
+
+    public String getM25() {
+        return M25;
+    }
+
+    public void setM25(String m25) {
+        M25 = m25;
+    }
+
+    public String getM26() {
+        return M26;
+    }
+
+    public void setM26(String m26) {
+        M26 = m26;
+    }
+
+    public String getM27() {
+        return M27;
+    }
+
+    public void setM27(String m27) {
+        M27 = m27;
+    }
+
+    public String getM28() {
+        return M28;
+    }
+
+    public void setM28(String m28) {
+        M28 = m28;
+    }
+
+    public String getM29() {
+        return M29;
+    }
+
+    public void setM29(String m29) {
+        M29 = m29;
+    }
+
+    public String getM30() {
+        return M30;
+    }
+
+    public void setM30(String m30) {
+        M30 = m30;
+    }
+
+    public String getM31() {
+        return M31;
+    }
+
+    public void setM31(String m31) {
+        M31 = m31;
+    }
+
+    public String getM32() {
+        return M32;
+    }
+
+    public void setM32(String m32) {
+        M32 = m32;
+    }
+
+    public String getM33() {
+        return M33;
+    }
+
+    public void setM33(String m33) {
+        M33 = m33;
+    }
+}
