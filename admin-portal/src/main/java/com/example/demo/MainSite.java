ecbdbc96d3a9233a0508a9ef64f4983c8fe78916
matveenko73 <andreymatveenko3@gmail.com>
2018-10-15 16:08:02 +0300

    копирую блоки по каждой машине отдельно

--- admin-portal/src/main/java/com/example/demo/MainSite.java
+++ admin-portal/src/main/java/com/example/demo/MainSite.java
@@ -12,7 +12,7 @@ public class MainSite {
                 "C:\\Users\\Андрей\\Downloads\\geckodriver.exe");
 
         FirefoxDriver driver = new FirefoxDriver();
-        driver.get("http://rst.ua/oldcars/bmw/");
+        driver.get("http://rst.ua/oldcars/?task=newresults&make%5B%5D=0&year%5B%5D=0&year%5B%5D=0&price%5B%5D=0&price%5B%5D=0&engine%5B%5D=0&engine%5B%5D=0&gear=0&fuel=0&drive=0&condition=0&from=sform");
 //        List<WebElement> urles = driver.findElementsByCssSelector(".rst-ocb-i-a");
 //        for (WebElement url : urles) {
 //            System.out.println((url.getAttribute("href")));
@@ -20,29 +20,56 @@ public class MainSite {
 
 //        System.out.println();
 //
-//        List<WebElement> pictures = driver.findElementsByCssSelector(".rst-ocb-i-i");
-//        for (WebElement picture : pictures) {
-//            System.out.println(picture.getAttribute("src"));
+        List<WebElement> urles = driver.findElementsByCssSelector(".rst-ocb-i-a");
+        List<WebElement> pictures = driver.findElementsByCssSelector(".rst-ocb-i-i");
+        List<WebElement> prices = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-s");
+        int k = 0;
+
+        for (int i = 0; i < pictures.size(); i++) {
+
+            System.out.println(pictures.get(i).getAttribute("src"));
+            System.out.println(urles.get(i).getAttribute("href"));
+            for (int j = 0; j < 6; j++) {
+                System.out.println(prices.get(k).getText());
+                k++;
+            }
+            System.out.println();
+        }
+
+//        for (int i = 0; i < pictures.size(); i++) {
+//
+//            for (WebElement j : pictures) {
+//                System.out.println(j.getAttribute("src"));
+//                continue;
+//            }
+//
+//            for (WebElement price : prices) {
+//                System.out.println(price.getText());
+//                continue;
+//            }
+//            System.out.println();
 //        }
+    }
+
 
 //        System.out.println();
 
-        List<WebElement> nextPages = driver.findElementsByCssSelector("ul.pagination a");
-        for (WebElement nextPage : nextPages) {
+//        List<WebElement> nextPages = driver.findElementsByCssSelector("ul.pagination a");
+//        for (WebElement nextPage : nextPages) {
 //            nextPage.findElements(By.cssSelector("324234234"));
-            nextPage.click();
+//            nextPage.click();
 
-            List<WebElement> prices = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-s");
-            int i = 0;
-            for (WebElement price : prices) {
-                i++;
-                if (i == 7) {
-                    i = 1;
-                    System.out.println();
-                }
-                System.out.println(price.getText());
-            }
-        }
-        //driver.close();
-    }
+//        List<WebElement> prices = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-s");
+//        int i = 0;
+//        for (WebElement price : prices) {
+//            i++;
+//            if (i == 7) {
+//                i = 1;
+//                System.out.println();
+//            }
+//            System.out.println(price.getText());
+//        }
+//    }
 }
+//driver.close();
+//rst-page-wrap
