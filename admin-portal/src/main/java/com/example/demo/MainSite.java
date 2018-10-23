6ba54d43f62050cdc5cb2ff61d4465502223cbfd
matveenko73 <andreymatveenko3@gmail.com>
2018-10-23 16:52:01 +0300

    распарсил по блокам с rst + состояние и область

--- admin-portal/src/main/java/com/example/demo/MainSite.java
+++ admin-portal/src/main/java/com/example/demo/MainSite.java
@@ -19,18 +19,24 @@ public class MainSite {
                 "C:\\Users\\Андрей\\Downloads\\geckodriver.exe");
 
         FirefoxDriver driver = new FirefoxDriver();
-        driver.get("http://rst.ua/oldcars/?task=newresults&make%5B%5D=0&year%5B%5D=0&year%5B%5D=0&price%5B%5D=0&price%5B%5D=0&engine%5B%5D=0&engine%5B%5D=0&gear=0&fuel=0&drive=0&condition=0&from=sform");
 
+        String str = "http://rst.ua/oldcars/?task=newresults&make%5B%5D=0&year%5B%5D=0&year%5B%5D=0&price%5B%5D=0&price%5B%5D=0&engine%5B%5D=0&engine%5B%5D=0&gear=0&fuel=0&drive=0&condition=0&from=sform&start=";
+        driver.get("http://rst.ua/oldcars/?task=newresults&make%5B%5D=0&year%5B%5D=0&year%5B%5D=0&price%5B%5D=0&price%5B%5D=0&engine%5B%5D=0&engine%5B%5D=0&gear=0&fuel=0&drive=0&condition=0&from=sform");
 
         List<WebElement> nextPages = driver.findElementsByCssSelector("ul.pagination a");
         List<WebElement> models = driver.findElementsByCssSelector(".rst-ocb-i-h");
         List<WebElement> urles = driver.findElementsByCssSelector(".rst-ocb-i-a");
         List<WebElement> pictures = driver.findElementsByCssSelector(".rst-ocb-i-i");
         List<WebElement> prices = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i");
+        List<WebElement> region = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-s");
 
         int k = 0;
         int o = 1;
         int u = 2;
+        int pageNomber = 1;
+        int re = 1;
+        int un = 3;
+
         try {
 
             for (int f = 0; f < 30; f++) {
@@ -38,36 +44,43 @@ public class MainSite {
 
                     System.out.println(pictures.get(i).getAttribute("src"));
                     System.out.println(urles.get(i).getAttribute("href"));
-                    System.out.println(models.get(i).getText());
+//                    System.out.println(models.get(i).getText());
+
+                    System.out.println(region.get(re).getText());
+                    System.out.println(region.get(un).getText());
+
+
+
                     String s = pictures.get(i).getAttribute("src");
                     int iz = s.indexOf("oldcars/");
-
                     String s2 = s.substring(iz + 8);
                     int x = s2.indexOf("/");
 
-                    String car = s2.substring(0, x);
-                    System.out.println(car);
+                    String brand = s2.substring(0, x);
+                    System.out.println(brand);
 
-                    s2 = s2.substring(car.length() + 1);
-                    s2 = s2.substring(0, s2.indexOf('/'));
-                    System.out.println(s2);
-
-//                        System.out.println(prices.get(i).getText());
-//                        System.out.println(prices.get(1).getText());
-//                        System.out.println(prices.get(2).getText());
-
+                    s2 = s2.substring(brand.length() + 1);
+                    String model = s2.substring(0, s2.indexOf('/'));
+                    System.out.println(model);
 
 //                    for (int j = 0; j < 3; j++) {
-
 //                        System.out.println(prices.get(k).getText());
-//
-//
+//                        k++;
+//                    }
+
                     String p = prices.get(k).getText();
                     int r = p.indexOf("Цена: ");
                     String r2 = p.substring(r + 6);
-                    int c = r2.indexOf("грн");
-                    String price = r2.substring(0, c);
-                    price = price.replaceAll("'", "");
+                    int c = r2.indexOf(" ");
+                    String price;
+                    if (c == -1) {
+
+                        price = "Договорная";
+                    } else {
+
+                        price = r2.substring(0, c);
+                        price = price.replaceAll("'", "");
+                    }
                     System.out.println(price);
 
 
@@ -78,50 +91,58 @@ public class MainSite {
                     String g2 = y.substring(g + 5);
                     int t = g2.indexOf(",");
                     String year = g2.substring(0, t);
-                    String prob = y.substring(l + 1, d);
+                    String mileage = y.substring(l + 1, d);
                     System.out.println(year);
-                    System.out.println(prob);
+                    System.out.println(mileage);
 
 
                     String z = prices.get(u).getText();
                     int w = z.indexOf("Двиг.: ");
                     String z2 = z.substring(w + 7);
                     int n = z2.indexOf(" ");
-                    String eng = z2.substring(0, n);
-                    System.out.println(eng);
-                    z2 = z2.substring(eng.length() + 1);
-                    String z3 = z2.substring(0, z2.indexOf(" "));
-                    System.out.println(z3);
+                    String engine = z2.substring(0, n);
+                    System.out.println(engine);
+                    z2 = z2.substring(engine.length() + 1);
+                    String fuel = z2.substring(0, z2.indexOf(" "));
+                    System.out.println(fuel);
                     int q = z2.indexOf("(");
                     int b = z2.indexOf(")");
-                    String z4 = z2.substring(q + 1, b);
-                    System.out.println(z4);
-//
+                    String transmission = z2.substring(q + 1, b);
+                    System.out.println(transmission);
+
                     k = k + 3;
                     o = o + 3;
                     u = u + 3;
+                    re = re +6;
+                    un = un +6;
 
-//                    }
                     System.out.println();
-//                    k = 0;
                 }
 //                new WebDriverWait(driver, 10).ignoring(StaleElementReferenceException.class);
 
-                WebElement nextPage = nextPages.get(f);
-                if (nextPage.getText().startsWith(">")) {
-                    f = 0;
-                }
+//                WebElement nextPage = nextPages.get(f);
+//                if (nextPage.getText().startsWith(">")) {
+//                    f = 0;
+//                }
 
-                nextPage.click();
+//                nextPage.click();
 
-                nextPages = driver.findElementsByCssSelector("ul.pagination a");
+                pageNomber++;
+                driver.get(str+pageNomber);
+
+
+//                nextPages = driver.findElementsByCssSelector("ul.pagination a");
                 models = driver.findElementsByCssSelector(".rst-ocb-i-h");
                 urles = driver.findElementsByCssSelector(".rst-ocb-i-a");
                 pictures = driver.findElementsByCssSelector(".rst-ocb-i-i");
                 prices = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i");
+                region = driver.findElementsByCssSelector(".rst-ocb-i-d-l-i-s");
+
                 k = 0;
                 o = 1;
                 u = 2;
+                re = 1;
+                un = 3;
 
             }
 //        } catch (StaleElementReferenceException e) {
--- admin-portal/src/main/java/com/example/demo/Test.java
+++ admin-portal/src/main/java/com/example/demo/Test.java
@@ -7,56 +7,65 @@ public class Test {
 //        Год: 2012, (93000 - пробег)
 //        Двиг.: 2.0 Бензин (Автомат)
 
-        String s = "http://img1.rstcars.com/oldcars/mitsubishi/eclipse/middle/8720292-2.jpg";
-        int i = s.indexOf("oldcars/");
-
-        String s2 = s.substring(i + 8);
-        int j = s2.indexOf("/");
-        System.out.println(s2);
-
-        String car = s2.substring(0, j);
-        System.out.println(car);
-
-        s2 = s2.substring(car.length() + 1);
-        s2 = s2.substring(0, s2.indexOf('/'));
-        System.out.println(s2);
-
+//        String s = "http://img1.rstcars.com/oldcars/mitsubishi/eclipse/middle/8720292-2.jpg";
+//        int i = s.indexOf("oldcars/");
+//
+//        String s2 = s.substring(i + 8);
+//        int j = s2.indexOf("/");
+//        System.out.println(s2);
+//
+//        String car = s2.substring(0, j);
+//        System.out.println(car);
+//
+//        s2 = s2.substring(car.length() + 1);
+//        s2 = s2.substring(0, s2.indexOf('/'));
+//        System.out.println(s2);
+//
         String p = "Цена: 606'000 грн $21'500";
         int r = p.indexOf("Цена: ");
         String r2 = p.substring(r + 6);
-        int c = r2.indexOf("грн");
+        int c = r2.indexOf(" ");
+        String price;
 
-        String price = r2.substring(0, c);
-        price = price.replaceAll("'", "");
+        if (c == -1) {
+
+            price = "Договорная";
+        } else {
+
+            price = r2.substring(0, c);
+            price = price.replaceAll("'", "");
+        }
+
         System.out.println(price);
+//
+//
+//        String y = "Год: 2012, (93000 - пробег)";
+//        int g = y.indexOf("Год: ");
+//        int l = y.indexOf("(");
+//        int d = y.indexOf("-");
+//        String g2 = y.substring(g + 5);
+//        int t = g2.indexOf(",");
+//
+//        String year = g2.substring(0, t);
+//        String prob = y.substring(l + 1, d);
+//        System.out.println(year);
+//        System.out.println(prob);
+//
+//        String z = "Двиг.: 2.0 Бензин (Автомат)";
+//        int x = z.indexOf("Двиг.: ");
+//        String z2 = z.substring(x + 7);
+//        int n = z2.indexOf(" ");
+//        String eng = z2.substring(0, n);
+//        System.out.println(eng);
+//
+//        z2 = z2.substring(eng.length() + 1);
+//        String z3 = z2.substring(0, z2.indexOf(" "));
+//        System.out.println(z3);
+//        int q = z2.indexOf("(");
+//        int b = z2.indexOf(")");
+//        String z4 = z2.substring(q + 1, b);
+//        System.out.println(z4);
 
-
-        String y = "Год: 2012, (93000 - пробег)";
-        int g = y.indexOf("Год: ");
-        int l = y.indexOf("(");
-        int d = y.indexOf("-");
-        String g2 = y.substring(g + 5);
-        int t = g2.indexOf(",");
-
-        String year = g2.substring(0, t);
-        String prob = y.substring(l + 1, d);
-        System.out.println(year);
-        System.out.println(prob);
-
-        String z = "Двиг.: 2.0 Бензин (Автомат)";
-        int x = z.indexOf("Двиг.: ");
-        String z2 = z.substring(x + 7);
-        int n = z2.indexOf(" ");
-        String eng = z2.substring(0, n);
-        System.out.println(eng);
-
-        z2 = z2.substring(eng.length() + 1);
-        String z3 = z2.substring(0, z2.indexOf(" "));
-        System.out.println(z3);
-        int q = z2.indexOf("(");
-        int b = z2.indexOf(")");
-        String z4 = z2.substring(q + 1, b);
-        System.out.println(z4);
 
     }
 }
