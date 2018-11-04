94853959a891a613f296bc7640150aca37a3730a
matveenko73 <andreymatveenko3@gmail.com>
2018-11-04 12:14:59 +0200

    добавили логику уже существующих в базе машин

--- admin-portal/src/main/java/com/example/demo/AdminPage.java
+++ admin-portal/src/main/java/com/example/demo/AdminPage.java
@@ -2,27 +2,67 @@ package com.example.demo;
 
 
 import com.example.demo.repositories.CarRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named
 public class AdminPage {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminPage.class);
 
     @Inject
     private CarRepository carRepository;
 
     public void runRST() {
         new RstTask((cars) -> {
-            carRepository.saveAll(cars);
-            System.out.println("Cars saved");
+            List<Car> newCars = updateExistingCarsInDataBase(cars);
+            carRepository.saveAll(newCars);
+            LOGGER.info("saved " + newCars.size() + " cars");
         }).run();
     }
+
     public void runAutoRia() {
         new AutoRiaTask((cars) -> {
             carRepository.saveAll(cars);
-            System.out.println("Cars saved");
+            LOGGER.info("Cars saved");
         }).run();
+    }
+
+    private List<Car> updateExistingCarsInDataBase(List<Car> cars) {
+        List<Car> newCars = new ArrayList<>();
+        for (Car car : cars) {
+            List<Car> alreadyExist = carRepository.findByMileageAndProductionYearAndEngineSize(car.getMileage(),
+                    car.getProductionYear(), car.getEngineSize());
+            if (!alreadyExist.isEmpty()) {
+                for (Car carExist : alreadyExist) {
+                    carExist.setInfo(car.getInfo());
+                    carExist.setTransmission(car.getTransmission());
+                    carExist.setState(car.getState() != null && car.getState().trim().length() > 0
+                            ? car.getState()
+                            : carExist.getState());
+
+                    carExist.setFuel(car.getFuel());
+                    carExist.setPrice(car.getPrice());
+                    carExist.setRegion(car.getRegion());
+                    carExist.setUrles(car.getUrles());
+                    carExist.setPictures(car.getPictures());
+                    carExist.setBrand(car.getBrand());
+                    carExist.setModel(car.getModel());
+                    carExist.setBody(car.getBody());
+                    carExist.setDriveUnit(car.getDriveUnit());
+                    carRepository.save(carExist);
+                    LOGGER.info("car was updated " + carExist.getMileage()
+                            + " " + carExist.getProductionYear() + " " + carExist.getEngineSize());
+                }
+            } else {
+                newCars.add(car);
+            }
+        }
+        return newCars;
     }
 
 }
--- admin-portal/src/main/java/com/example/demo/repositories/CarRepository.java
+++ admin-portal/src/main/java/com/example/demo/repositories/CarRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repositories;
 import com.example.demo.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car>  findByMileageAndProductionYearAndEngineSize(Integer mileage, Integer productionYear, String engineSize);
 }
